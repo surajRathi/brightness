1f5909815bec471ec22b78179fa325358fd73fa9
Suraj Rathi <suraj.rathi00@gmail.com>
2020-08-19 23:05:55 +0530

    Modernized and rewrote log function

--- brightness.cpp
+++ brightness.cpp
@@ -12,51 +12,71 @@
 
 namespace fs = std::experimental::filesystem;
 
-const char *base = "/sys/class/backlight/";
-const float min_percent_frac = 0.01;
-const float exp_factor = 40; // 50;
+const char BASE_PATH[] = "/sys/class/backlight/";
+const float MIN_PERCENT_FRAC = 0.01;
+
 // TODO: constexpr
-const float denom = std::exp(100 / exp_factor) - 1;
+//const float exp_factor = 40; // 50;
+//const float denom = std::exp(100 / exp_factor) - 1;
+const float log_factor = 2;
+static const float logged_factor = std::log(1 + log_factor);
 
-const std::string newline = "\n";
-const std::string usage = \
-    "Usage: brightness [OPTION] [NEW_VALUE]" + newline +
-    "Used to get and set the brightness of backlight devices." + newline +
-    "" + newline +
-    "Mandatory arguments to long options are mandatory for short options too." + newline +
-    "    -d, --device=DEV	specify the device name," + newline +
-    "			  if none give, uses the first device" + newline +
-    "    -h, --help		display this help and exit" + newline +
-    "    -m, --max		display max brightness values too" + newline +
-    "" + newline +
-    "VALUE should be of the form:" + newline +
-    "    [+,-]NUMBER[%]" + newline +
-    "" + newline +
-    "";
+const char USAGE[] = \
+    "Usage: brightness [OPTIONS] [NEW_VALUE]\n"
+    "Used to get_brightness and set the brightness of backlight devices.\n"
+    "\n"
+    "Mandatory arguments to long options are mandatory for short options too.\n"
+    "    -d, --device=DEV	specify the device name,\n"
+    "			            if none give, uses the first available device\n"
+    "    -h, --help		display this help and exit\n"
+    "\n"
+    "NEW_VALUE should be of the form:\n"
+    "    [+,-]NUMBER[%]\n";
 
 
-std::tuple<int, int> get(std::string path) {
-    if ((char) path.back() != '/')
-        path += "/";
+std::string_view get_device(const std::string &path) {
+    auto dev = std::string_view(path);
+    dev.remove_prefix(path.find_last_of('/') + 1);
+    return dev;
+}
+
+std::string get_path(const std::string &device_spec) {
+    for (const auto &entry : fs::directory_iterator(BASE_PATH)) {
+        auto path = entry.path().string();
+
+        if (device_spec.empty() || (get_device(path) == device_spec))
+            return path;
+    }
+    return {};
+}
+
+std::pair<int, int> get_brightness(std::string path) {
     int cur, max;
-    std::ifstream(path + "brightness") >> cur;
-    std::ifstream(path + "max_brightness") >> max;
-    return std::make_tuple(cur, max);
+    if ((char) path.back() == '/') {
+        std::ifstream(path + "brightness") >> cur;
+        std::ifstream(path + "max_brightness") >> max;
+    } else {
+        std::ifstream(path + "/brightness") >> cur;
+        std::ifstream(path + "/max_brightness") >> max;
+    }
+    return std::make_pair(cur, max);
 }
 
 int percent_to_brightness(float percent, int max_brightness) {
-    return static_cast<int>((std::exp(percent / 100) - 1) / denom * max_brightness);
+    if (percent > 1) percent /= 100; // Dirty hack for xx% and 0.xx
+
+    return static_cast<int>(static_cast<float>(max_brightness) * std::log(1 + log_factor * percent) / logged_factor);
 }
 
 float brightness_to_percent(float brightness, int max_brightness) {
-    return std::log((brightness / max_brightness) * denom + 1) * 100;
+    return (std::exp(logged_factor * brightness / static_cast<float>(max_brightness)) - 1) / log_factor;
 }
 
-void display(std::string path, const std::string &device, bool display_max = false) {
-    // TODO: remove display_max;
-    auto[cur, max] = get(path);
-    // std::cout << device << ":\t" << 100*cur / max << "%"  << (display_max? " of " + to_std::string(max) : "") << std::endl;
-    std::cout << device << ":\t" << (int) brightness_to_percent(cur, max) << "%" << std::endl;
+void display(const std::string &path) {
+    auto device = get_device(path);
+    auto[cur, max] = get_brightness(path);
+    std::cout << device << ":\t" << static_cast<int>(std::ceil(100 * brightness_to_percent(cur, max))) << "%"
+              << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -65,35 +85,41 @@ int main(int argc, char *argv[]) {
     std::string value;  // brightness value. should be -1 if unset
     if (argc == 2) value = argv[1];
 
-    std::string device_spec; // should be set by options
-    bool display_max = false;
-
-    if ((!value.empty()) && (device_spec.length() == 0))
-        device_spec = "_";
-
-    std::string path;
-    std::string device;
-    for (const auto &entry : fs::directory_iterator(base)) {
-        path = entry.path();
-        device = path.substr(path.find_last_of('/') + 1);
-        if (value.empty()) display(path, device, display_max);
-        if ((device_spec == "_") || (device == device_spec)) break;
-        path = "";
+    if (value == "-h" || value == "--help") {
+        std::cout << USAGE;
+        return 0;
     }
-    if (value.empty()) return 0; // End of default mode.
-    if (path.length() == 0) return 1; // Device not found.
 
-    path += "/";
-    auto[cur, max] = get(path);
+    // TODO: set by options
+    std::string device_spec;
+
+    std::string path = get_path(device_spec);
+
+
+    if (path.empty()) {
+        if (device_spec.empty())
+            std::cout << "No backlight found." << std::endl;
+        else
+            std::cout << "Backlight '" << device_spec << "' not found." << std::endl;
+        return 1;
+    }
+
+    if (value.empty()) {
+        display(path);
+        return 0; // End of default mode.
+    }
+
+    auto[cur, max] = get_brightness(path);
+
     int new_value = 0;
     if ((char) value.back() == '%') {
         switch (value[0]) {
             case '+' :
             case '-' :
-                new_value = percent_to_brightness(brightness_to_percent(cur, max) + stoi(value), max);
+                new_value = percent_to_brightness(100 * brightness_to_percent(cur, max) + std::stof(value), max);
                 break;
             default:
-                new_value = percent_to_brightness(stoi(value), max);
+                new_value = percent_to_brightness(std::stof(value), max);
         }
     } else {
         switch (value[0]) { // value.back() vs value[value.length() - 1]
@@ -105,11 +131,12 @@ int main(int argc, char *argv[]) {
                 new_value = stoi(value);
         }
     }
-    if (new_value < min_percent_frac * max) new_value = min_percent_frac * max;
+
+    if (new_value < MIN_PERCENT_FRAC * max) new_value = MIN_PERCENT_FRAC * max;
     if (new_value > max) new_value = max;
 
-    std::ofstream(path + "brightness") << new_value;
-    display(path, device);
+    std::ofstream(path + "/brightness") << new_value;
 
+    display(path);
     return 0;
 }
